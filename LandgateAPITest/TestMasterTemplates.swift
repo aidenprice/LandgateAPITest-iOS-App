5f975beba21de5b63f0ab83903624c45cb941707
aidenprice <aidenprice@gmail.com>
2015-11-09 19:37:39 +1100

    First Icon sets added. Structs in Templates now have their variable names added.

--- LandgateAPITest/TestMasterTemplates.swift
+++ LandgateAPITest/TestMasterTemplates.swift
@@ -20,38 +20,38 @@ struct Template {
 
 struct TestMaster {
 	static let Standard = Template(
-		"Standard Test",
-		"The basic test format and a good all-rounder. " +
+		name: "Standard Test",
+		details: "The basic test format and a good all-rounder. " +
 		"Tests a variety of endpoints; GeoJSON, WMS, WMTS. " +
 		"Asks for small responses for the most part to minimise " +
 		"data downloads. One in 10 requests is much larger as a " +
 		"control on network latency on response time.",
-		1,
-		""
+		downloadSize: 1,
+		image: UIImage(named: "Standard Icon")!
 	)
 	static let ExtraLong = Template(
-		"Extra Long Test",
-		"",
-		1,
-		""
+		name: "Extra Long Test",
+		details: "",
+		downloadSize: 1,
+		image: UIImage(named: "Extra Long Icon")!
 	)
 	static let RemoteArea = Template(
-		"Remote Area Test",
-		"",
-		1,
-		""
+		name: "Remote Area Test",
+		details: "",
+		downloadSize: 1,
+		image: UIImage(named: "Remote Area Icon")!
 	)
 	static let WMS = Template(
-		"WMS Test",
-		"",
-		1,
-		""
+		name: "WMS Test",
+		details: "",
+		downloadSize: 1,
+		image: UIImage(named: "WMS Icon")!
 	)
 	static let WiFi = Template(
-		"WiFi Test",
-		"",
-		1,
-		""
+		name: "WiFi Test",
+		details: "",
+		downloadSize: 1,
+		image: UIImage(named: "WiFi Icon")!
 	)
 }
 
